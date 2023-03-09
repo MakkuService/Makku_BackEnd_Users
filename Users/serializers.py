e527d62be606940d2a7942030c683c646ee01cd3
Akumsk <Akumsk.ak@gmail.com>
2023-03-09 11:19:37 +0800

    fix_migration

--- Users/serializers.py
+++ Users/serializers.py
@@ -35,7 +35,7 @@ class EmailVerificationSerializer(serializers.ModelSerializer):
 
 
 class LoginSerialaizer(serializers.ModelSerializer):
-    email = serializers.EmailField(max_length=255)
+    email = serializers.EmailField(max_length=255, min_length=6,)
     password = serializers.CharField(max_length=70, min_length=6, write_only=True)
     username = serializers.CharField(max_length=70, min_length=6, read_only=True)
     tokens = serializers.CharField(max_length=70, min_length=6, read_only=True)
